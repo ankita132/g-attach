335de8c1b82fecc0fae96a4b1d8005fe49954a35
akash123mandal <akash123mandal@gmail.com>
2018-03-09 18:19:34 +0530

    Adding nothing that works

--- background.js
+++ background.js
@@ -1,1 +1,64 @@
+//oauth2 auth
+chrome.identity.getAuthToken(
+    {'interactive': true},
+    function(){
+        //load Google's javascript client libraries
+        window.gapi_onload = authorize;
+        loadScript('https://apis.google.com/js/client.js');
+    }
+);
+
+function loadScript(url){
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function(){
+        if(request.readyState !== 4) {
+            return;
+        }
+
+        if(request.status !== 200){
+            return;
+        }
+
+        eval(request.responseText);
+    };
+
+    request.open('GET', url);
+    request.send();
+}
+
+function authorize(){
+    gapi.auth.authorize(
+        {
+            client_id: '692519634641-0q028cvd90d6s9teen0lg22l6treiqcu.apps.googleusercontent.coms',
+            immediate: true,
+            scope: 'https://www.googleapis.com/auth/gmail.modify'
+        },
+        function(){
+            gapi.client.load('gmail', 'v1', gmailAPILoaded);
+        }
+    );
+}
+
+function gmailAPILoaded(){
+
+}
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(sender.tab ?
+            "from a content script:" + sender.tab.url :
+            "from the extension");
+        if (request.greeting == "hello") {
+            var request = gapi.client.gmail.users.drafts.list({
+                'userId': 'me'
+            });
+            request.execute(function(resp) {
+                console.log(resp);
+            });
+            sendResponse({farewell: "goodbye"});
+        }
+    });
+
+
 
--- content.js
+++ content.js
@@ -1,22 +1,30 @@
-setTimeout(function(){$(".TK").append('\
-<div class="aim">\
-    <div class="TO" id=":i5">\
-        <div class="TN bzz aHS-bnw" style="margin-left:0px">\
-            <div class="qj qr">\
-            </div>\
-            <div class="aio UKr6le">\
-                <span class="nU "><a href="https://mail.google.com/mail/u/0/#starred" target="_top" class="J-Ke n0" title="Starred" tabindex="-1" id="attachments">Attachments</a></span>\
-            </div>\
-            <div class="nL aif">\
+
+setTimeout(function(){
+    $(".TK:first").append('\
+    <div class="aim">\
+        <div class="TO" id=":i5">\
+            <div class="TN bzz aHS-bnw" style="margin-left:0px">\
+                <div class="qj qr">\
+                </div>\
+                <div class="aio UKr6le">\
+                    <span class="nU ">\
+                        <a href="https://mail.google.com/mail/u/0/#starred" target="_top" class="J-Ke n0" title="Starred" tabindex="-1" id="attachments">Attachments</a>\
+                    </span>\
+                </div>\
+                <div class="nL aif">\
+                </div>\
             </div>\
         </div>\
-    </div>\
-</div>');
-$("#attachments").on('click', function(){
-    $(".AO").children().hide();
-});
-$(".J-Ke.n0").on('click', function(){
-    $(".AO").children().show();
-    console.log("Showing");
-});
+    </div>');
+    $("#attachments").on('click', function(){
+        $(".AO").children().hide();
+        var wrapper = "<div class='attach-wrap'></div>";
+        //$(".AO").html(wrapper);
+        chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+            console.log(response.farewell);
+        });
+    });
+    $(".J-Ke.n0").not("#attachments").on('click', function(){
+        $(".AO").children().show();
+    });
 }, 6000);
